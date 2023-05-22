36f8d2a37ef886303d0c01de83d4eb0cd0fdc381
StanleyGunz <stanislavkinsler@gmail.com>
2023-05-22 15:17:24 +0700

    first commit

--- src/main/java/ru/gb/lesson_one/main/Main.java
+++ src/main/java/ru/gb/lesson_one/main/Main.java
@@ -1,2 +1,16 @@
-package ru.gb.lesson_one.main;public class Main {
+package ru.gb.lesson_one.main;
+
+import ru.gb.lesson_one.system.FormatSystem;
+import ru.gb.lesson_one.system.BasicMathSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = BasicMathSystem.add(2, 5);
+        print(result);
+    }
+
+    public static void print(int x) {
+        System.out.println(FormatSystem.format(x));
+    }
+
 }
--- src/main/java/ru/gb/lesson_one/system/FormatSystem.java
+++ src/main/java/ru/gb/lesson_one/system/FormatSystem.java
@@ -1,2 +1,17 @@
-package ru.gb.lesson_one.system;public class FormatSystem {
+package ru.gb.lesson_one.system;
+
+
+/**
+ *  Класс, который приводить программе эстетический вид.
+ */
+
+public class FormatSystem {
+    public static String format(int x) {
+        /*
+         -  Метод приводит результат мат.действия в формат строк
+         */
+
+        return String.format("Answer is : %d",x);
+    }
+
 }
